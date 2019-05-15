4e00aa047469248ab85df80fc8830a0a025b3b0c
Gao Xinrui <xinrui98@users.noreply.github.com>
2019-05-15 14:21:28 +0800

    Edited pictures, used them from pixar, not unsplash

--- lib/music_repo.dart
+++ lib/music_repo.dart
@@ -5,7 +5,7 @@ class MusicRepo {
   List allMusic = [
     Music("Dreaming", "assets/l2_3.mp3", "assets/dreaming.jpg",defaultText),
     Music("Fountain Bubbling", "assets/l1_1.mp3","assets/fountain_bubbling.jpg",defaultText),
-    Music("Harmony", "assets/l2_1.mp3", "assets/harmony1.jpg", defaultText),
+    Music("Harmony", "assets/l2_1.mp3", "assets/harmony.jpg", defaultText),
     Music("Gentle Lake", "assets/l1_2.mp3", "assets/gentle_lake.jpg",defaultText),
     Music("Ocean Waves", "assets/l1_3.mp3", "assets/ocean_waves.jpg",defaultText),
     Music("Tranquility", "assets/l2_2.mp3", "assets/tranquility.jpg",defaultText),
